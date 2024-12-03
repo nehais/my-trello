bdbbe3d7ead9de4248a1af45a20c8510329effd5
nehais <nehasinha82@gmail.com>
2024-12-03 15:14:24 +0100

    Edit task & add activity button

--- src/app/workspace/task-list/task-list.component.ts
+++ src/app/workspace/task-list/task-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Task } from 'src/app/models/task';
+import { taskActivity } from 'src/app/models/task-activity';
 
 @Component({
   selector: 'app-task-list',
@@ -9,8 +10,13 @@ import { Task } from 'src/app/models/task';
 export class TaskListComponent {
   @Input()
   task: Task | undefined;
+  newAct: boolean = false;
 
-  addList() {
-    let i = 10;
+  addList() {}
+
+  addNewAct() {
+    let newTask: taskActivity = { taskActId: 10, name: '', desc: '' };
+    this.task?.activities.push(newTask);
+    this.newAct = true;
   }
 }
--- src/app/workspace/workspace.module.ts
+++ src/app/workspace/workspace.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { WorkspaceComponent } from './workspace/workspace.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
 import { TaskListComponent } from './task-list/task-list.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [WorkspaceComponent, SideBarComponent, TaskListComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule],
   exports: [SideBarComponent],
 })
 export class WorkspaceModule {}
