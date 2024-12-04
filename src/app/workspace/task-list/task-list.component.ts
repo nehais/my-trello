6f02908346d71854f8da1ca1ded5fe224f30015d
nehais <nehasinha82@gmail.com>
2024-12-04 12:13:51 +0100

    Add input to the tile & activity name

--- src/app/workspace/task-list/task-list.component.ts
+++ src/app/workspace/task-list/task-list.component.ts
@@ -11,12 +11,21 @@ export class TaskListComponent {
   @Input()
   task: Task | undefined;
   newAct: boolean = false;
+  newTask: boolean = false;
 
-  addList() {}
+  addList() {
+    let newTask: Task = {
+      taskId: 10,
+      taskTitle: '',
+      activities: [{ taskActId: 10, name: '', desc: '' }],
+    };
+    this.task = newTask;
+    this.newTask = true;
+  }
 
   addNewAct() {
-    let newTask: taskActivity = { taskActId: 10, name: '', desc: '' };
-    this.task?.activities.push(newTask);
+    let newTaskAct: taskActivity = { taskActId: 10, name: '', desc: '' };
+    this.task?.activities.push(newTaskAct);
     this.newAct = true;
   }
 }
--- src/app/workspace/workspace.module.ts
+++ src/app/workspace/workspace.module.ts
@@ -5,10 +5,20 @@ import { SideBarComponent } from './side-bar/side-bar.component';
 import { TaskListComponent } from './task-list/task-list.component';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [WorkspaceComponent, SideBarComponent, TaskListComponent],
-  imports: [CommonModule, MatButtonModule, RouterModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    RouterModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   exports: [SideBarComponent],
 })
 export class WorkspaceModule {}
